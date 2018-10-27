5910f9f87190a9a31bb26f1e6f333138175f0281
Sam007961 <24758349+sam007961@users.noreply.github.com>
2018-10-28 01:19:16 +0200

    started graph class.

--- Practice/Graph/Graph.h
+++ Practice/Graph/Graph.h
@@ -1,1 +1,26 @@
 #pragma once
+#include <vector>
+#include <list>
+
+struct Edge {
+	int src, dest, weight;
+};
+
+struct Graph {
+	std::vector<std::list<int>> adjList;
+
+	Graph(const Edge* edges) {
+
+	}
+
+	Graph(const std::vector<Edge>& edges) {
+
+	}
+
+};
+
+
+/* References
+        https://www.geeksforgeeks.org/graph-data-structure-and-algorithms/
+        http://www.techiedelight.com/graph-implementation-using-stl/
+*/
